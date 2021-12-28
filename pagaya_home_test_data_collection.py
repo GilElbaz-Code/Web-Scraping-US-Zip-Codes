200a0d9046b99ade08ab784dc33c586214a7aeb6
Gil <elbazg123@gmail.com>
2021-12-28 22:51:02 +0200

    pandas

--- pagaya_home_test_data_collection.py
+++ pagaya_home_test_data_collection.py
@@ -1,27 +1,72 @@
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
-import calendar
-import datetime
+import pandas as pd
+import re, json
+import threading
+
+FIRST_ZIP_CODE = 601
+LAST_ZIP_CODE = 99950
+
+zip_to_coords = {}
+threads = []
+
+
+def request_zipcode(code):
+    global zip_to_coords
+    results = []
+    columns = ["zip"]
+    with requests.Session() as s:
+        s.headers = {'User-Agent': 'Mozilla/5.0'}
+        url = f'https://www.unitedstateszipcodes.org/{str(code).zfill(5)}/#stats'
+        r = s.get(url)
+        res = re.findall(r'var data = (\[.*\])', r.text)
+        relevant = [data for data in res if json.loads(data)[0]["values"][0]["x"] == 2005]
+        if len(relevant) > 0:
+            relevant = json.loads(relevant[0])[0]["values"]
+            try:
+                data = relevant
+                values = [i['y'] for i in data]
+                values.insert(0, code)
+                results.append(values)
+                if values:
+                    columns.extend([i['x'] for i in data])
+                zip_to_coords[code] = (columns, *results)
+                print(f"{code}={zip_to_coords[code]}")
+            except:
+                pass
+        else:
+            zip_to_coords[code] = None
+
+
+for i in range(23022, 23100):
+    t = threading.Thread(target=request_zipcode, args=(i,), daemon=True)
+    threads.append(t)
+    t.start()
+
+for t in threads:
+    t.join()
+
+# pd.options.display.max_columns = None
+# pd.options.display.max_rows = None
+# df = pd.DataFrame(results, columns=columns)
+# print(df)
+
 
 '''
-1) Extract data from web. - Hard
-2) Insertion to Pandas dataframe. - Easy
-'''
-
 url = 'https://www.unitedstateszipcodes.org/23022/#stats'
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
-                         'Chrome/50.0.2661.102 Safari/537.36'}
+headers = {'User-Agent': 'Chrome/50.0.2661.102'}
 page = requests.get(url, headers=headers)
 soup = BeautifulSoup(page.content, 'html.parser')
-# print(soup.prettify())
 
+links = soup.find_all('a', href=True)
+'''
 
+'''
 years = ['Historical ' + str(year) for year in range(2005, 2019)]
 
-columns = ['ZIP Code', 'Current Population', *years]
+columns = ['ZIP Code', *years]
 df = pd.DataFrame(columns=columns)
 print(df)
-
+'''
 if __name__ == '__main__':
     pass
